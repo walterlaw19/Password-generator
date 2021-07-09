a2aa63322f2e083978da3a3a99cd83fd83f442c2
SC2021 <walterlaw19@gmail.com>
2021-07-09 19:35:57 -0400

    add lower case and uppper case arrays

--- Develop/script.js
+++ Develop/script.js
@@ -5,35 +5,29 @@ function generatePassword() {
   // array of special characters
   var specialCharacters = ["#", "@", "!", "*","&", "$", "%", "+"];
 
-  //  let sc = specialCharacters.length;  
-
-  // for (let i = 0; i < sc; i++) {
-  //   text += ""
-
-  //  }
-
-   // 
-  
-
   // array of numbers
-  var numbers = ["1", "2", "3", "4", "5", "6"];
-  console.log(numbers);
-  console.log(numbers[2]);
-
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  
   // array of lower case letters
-  var lowerCaseLetters = ["a", "b", "c"];
-  console.log(lowerCaseLetters);
-
+  var lowerCaseLetters = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
   // array of upper case letters
-  var upperCaseLetters = ["A", "B", "C"];
-  console.log(upperCaseLetters);
+  var upperCaseLetters = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
 
   // empty array of characters
   var emptyCharacters = [];
-  console.log(emptyCharacters);
+
+
+
+
+
+
+
+
+
 
   //ask the user how many characters
-  var howManyCharacters = window.prompt("how many characters you want to include in this password?");
+  var howManyCharacters = window.prompt("how many characters you want to include in this password? ENTER a number from 8 to 125");
   // answers from window.promt is always of datatype STRING
 
   // parseInt converts a STRING datatype to an number data type
@@ -41,13 +35,26 @@ function generatePassword() {
   // "10" + "11" = "1011"
   // 10 + 11 = 21
 
-  // check if the answer is a number
+  // check if the answer is a number - TUTOR!!!
   
+
 
   // checks if number is within range
   while(parseInt(howManyCharacters) < 8 || parseInt(howManyCharacters)  > 128) {
     howManyCharacters = window.prompt("how many characters you want to include in this password?");
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   //ask the user if they want numbers
@@ -67,35 +74,32 @@ function generatePassword() {
     emptyCharacters = emptyCharacters.concat(numbers);
   }
   
-
   // if sc, append special characters to character array
-
   if (anySpecialCharacters) {
     emptyCharacters = emptyCharacters.concat(specialCharacters)
   }
 
   // if ul, append upper case letetrs to characters array
-
+  if (anyUpperCaseLetters) {
+    emptyCharacters = emptyCharacters.concat(upperCaseLetters);
+  }
 
   // if ll, append lowercase letters
-
+  if (anyLowerCaseLetters) {
+    emptyCharacters = emptyCharacters.concat(lowerCaseLetters);
+  }
 
   //generate the password
   console.log(emptyCharacters);
 
-
   var randomCharacters = "";
   
   //repeat based on number of desired characters
-  // howManyCharacters
+  
   for (let index = 0; index < howManyCharacters; index++) {
     
     //add random character from character array to password
-    var randomNumber= Math.floor(Math.random() * emptyCharacters.length); /// random number between 0 -> 1 ... 0.12431231, 0.97457654, 0.9999999
-    // 1.36  -> 1
-    // 10.7  --> 10
-    // 10.9999999999 --> 10
-    // 0 - 10
+    var randomNumber= Math.floor(Math.random() * emptyCharacters.length); 
 
     var randomChara = emptyCharacters[randomNumber]
 
@@ -105,13 +109,17 @@ function generatePassword() {
   console.log(randomCharacters)
   
 
-
-
-
-
   //return the password
   return randomCharacters;
 }
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
